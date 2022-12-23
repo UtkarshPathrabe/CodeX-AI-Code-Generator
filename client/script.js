3a3f9f4a5d3d13a6d4a62d3c3316452c2c4cb316
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-12-23 21:27:10 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -63,7 +63,7 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId);
   loader(messageDiv);
   // fetch data from server
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch(import.meta.env.VITE_SERVER_URL, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
